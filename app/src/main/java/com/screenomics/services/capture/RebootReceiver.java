c6d2ec02050a87485c5095d50c255ec3310dc0c0
Ryan Yu <ryanyudev@gmail.com>
2024-03-27 16:44:55 +0800

    feat: add reboot receiver

---
+++ app/src/main/java/com/screenomics/services/capture/RebootReceiver.java
@@ -0,0 +1,41 @@
+package com.screenomics.services.capture;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import com.screenomics.R;
+
+public class RebootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent serviceIntent = new Intent(context, CaptureActivity.class);
+        int intentFlags;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            intentFlags = PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT;
+        } else {
+            intentFlags = PendingIntent.FLAG_UPDATE_CURRENT;
+        }
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, serviceIntent,
+                intentFlags);
+
+        Notification notification =
+                new Notification.Builder(context, "reminder-channel")
+                        .setContentTitle("Please resume capture")
+                        .setContentText("It seems like you have restarted your device. Please " +
+                                "resume capture ASAP!")
+                        .setSmallIcon(R.drawable.dna)
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+        NotificationManager notificationManager =
+                context.getSystemService(NotificationManager.class);
+        notificationManager.notify(5, notification);
+    }
+}
